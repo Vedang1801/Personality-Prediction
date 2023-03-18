33963916f5a644d6688d1bc60275dd84234c10c6
Vedang Malusare <74106949+Vedang1801@users.noreply.github.com>
2023-03-18 15:20:02 +0530

    except block

--- Personality.py
+++ Personality.py
@@ -11,15 +11,14 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 # Define the file paths
 MODEL_FILE_PATH = 'Models/model_cat.pkl'
 
-st.set_page_config(page_title="Personality-Prediction",layout="centered",  page_icon="🧠")
+st.set_page_config(page_title="Personality-Prediction",layout="centered", page_icon="🧠")
 # Define the Streamlit app
-
 
 def app():
     # Add a title to the app
     st.title("Personality Prediction Questionnaire")
     st.write("-----------------------------------------------------")
-
+    
     # Define the questions and options
     questions = [
         "Question 1: I enjoy being in large groups of people.",
@@ -76,10 +75,11 @@ def app():
     st.write("-----------------------------------------------------")
     st.header("Personality Type Predictor")
     st.caption("Click On The Predict Button to know your Personality")
+    
 
     # Make a prediction and display the result
     st.write(" ")
-    if st.button('PREDICT :thought_balloon:'):
+    if st.button('**PREDICT** :thought_balloon:'):
         try:
             # Load the model
             loaded_model = load_model(MODEL_FILE_PATH)
@@ -96,22 +96,22 @@ def app():
             # Define a switch case function
             def switch_case(y):
                 switcher = {
-                    0: "ENFJ(The Teacher): Warm and empathetic, ENFJs are natural leaders who bring out the best in others. They are excellent communicators and know how to bring harmony to a group. ENFJs are driven by a sense of purpose and a desire to make a positive impact on the world.",
-                    1: "ENFP(The Champion): Enthusiastic and optimistic, ENFPs are full of ideas and always ready for an adventure. They are excellent at understanding and connecting with others, and enjoy bringing people together. ENFPs are highly creative and often excel at finding new and innovative solutions to problems.",
-                    2: "ENTJ(The Commander): Strategic and driven, ENTJs are natural leaders who excel at creating and executing plans. They are confident and assertive, and are not afraid to make tough decisions. ENTJs are highly ambitious and always looking for new challenges to conquer.",
-                    3: "ENTP(The Visionary): Quick-witted and always curious, ENTPs are natural problem-solvers who enjoy exploring new ideas and possibilities. They are enthusiastic and energetic, and are always looking for ways to shake things up. ENTPs are highly independent and value their freedom to think and act as they please.",
-                    4: "ESFJ(The Provider): Warm and supportive, ESFJs are natural caregivers who enjoy helping others. They are highly organized and reliable, and value traditions and stability. ESFJs are driven by a strong sense of responsibility and a desire to make a positive impact on those around them.",
-                    5: "ESFP(The Performer: Fun-loving and spontaneous, ESFPs are the life of the party and enjoy being in the spotlight. They are highly social and enjoy making connections with others. ESFPs live in the moment and value their freedom to enjoy life to the fullest.",
-                    6: "ESTJ(The Supervisor): Practical and down-to-earth, ESTJs are natural leaders who are highly organized and efficient. They value tradition and order, and are driven by a sense of duty and responsibility. ESTJs are highly assertive and confident, and are not afraid to take charge in a crisis.",
-                    7: "ESTP(The Dynamo): Bold and daring, ESTPs are natural risk-takers who enjoy living life on the edge. They are highly action-oriented and enjoy taking control of a situation. ESTPs value their independence and enjoy exploring new experiences and opportunities.",
-                    8: "INFJ(The Counselor): Intuitive and empathetic, INFJs are natural healers who enjoy helping others. They are highly imaginative and often have a strong sense of their life's purpose. INFJs are driven by a strong sense of morality and a desire to make a positive impact on the world.",
-                    9: "INFP(The Idealist): Idealistic and creative, INFPs are natural dreamers who enjoy exploring their own thoughts and feelings. They are highly empathetic and value deep connections with others. INFPs are driven by a strong sense of purpose and a desire to make a positive impact on the world.",
-                    10: "INTJ(The Architect): Strategic and visionary, INTJs are natural leaders who excel at developing and executing plans. They are highly analytical and enjoy finding new and innovative solutions to problems. INTJs are driven by a strong sense of purpose and a desire to make a positive impact on the world.",
-                    11: "INTP(The Architect): They are creative, innovative, and logical problem-solvers. They enjoy exploring new ideas and theories, and are not afraid to challenge traditional thinking.",
-                    12: "ISFJ(The Protector): They are responsible, reliable, and nurturing individuals. They have a strong sense of duty and put the needs of others before their own. They value tradition and stability.",
-                    13: "ISFP(The Composer): They are sensitive, imaginative, and spontaneous individuals. They are often artistic, and enjoy living in the moment. They value their independence and freedom.",
-                    14: "ISTJ(The Inspector): They are practical, dependable, and methodical individuals. They value order, stability, and structure, and are committed to their responsibilities. They are often the backbone of organizations.",
-                    15: "ISTP(The Craftsman): They are hands-on individuals who enjoy taking things apart and figuring out how they work. They are confident, independent, and resourceful, and value action and results over theoretical ideas."
+                    0: "*ENFJ(The Teacher)*: Warm and empathetic, ENFJs are natural leaders who bring out the best in others. They are excellent communicators and know how to bring harmony to a group. ENFJs are driven by a sense of purpose and a desire to make a positive impact on the world.",
+                    1: "*ENFP(The Champion)*: Enthusiastic and optimistic, ENFPs are full of ideas and always ready for an adventure. They are excellent at understanding and connecting with others, and enjoy bringing people together. ENFPs are highly creative and often excel at finding new and innovative solutions to problems.",
+                    2: "*ENTJ(The Commander)*: Strategic and driven, ENTJs are natural leaders who excel at creating and executing plans. They are confident and assertive, and are not afraid to make tough decisions. ENTJs are highly ambitious and always looking for new challenges to conquer.",
+                    3: "*ENTP(The Visionary)*: Quick-witted and always curious, ENTPs are natural problem-solvers who enjoy exploring new ideas and possibilities. They are enthusiastic and energetic, and are always looking for ways to shake things up. ENTPs are highly independent and value their freedom to think and act as they please.",
+                    4: "*ESFJ(The Provider)*: Warm and supportive, ESFJs are natural caregivers who enjoy helping others. They are highly organized and reliable, and value traditions and stability. ESFJs are driven by a strong sense of responsibility and a desire to make a positive impact on those around them.",
+                    5: "*ESFP(The Performer*: Fun-loving and spontaneous, ESFPs are the life of the party and enjoy being in the spotlight. They are highly social and enjoy making connections with others. ESFPs live in the moment and value their freedom to enjoy life to the fullest.",
+                    6: "*ESTJ(The Supervisor)*: Practical and down-to-earth, ESTJs are natural leaders who are highly organized and efficient. They value tradition and order, and are driven by a sense of duty and responsibility. ESTJs are highly assertive and confident, and are not afraid to take charge in a crisis.",
+                    7: "*ESTP(The Dynamo)*: Bold and daring, ESTPs are natural risk-takers who enjoy living life on the edge. They are highly action-oriented and enjoy taking control of a situation. ESTPs value their independence and enjoy exploring new experiences and opportunities.",
+                    8: "*INFJ(The Counselor)*: Intuitive and empathetic, INFJs are natural healers who enjoy helping others. They are highly imaginative and often have a strong sense of their life's purpose. INFJs are driven by a strong sense of morality and a desire to make a positive impact on the world.",
+                    9: "*INFP(The Idealist)*: Idealistic and creative, INFPs are natural dreamers who enjoy exploring their own thoughts and feelings. They are highly empathetic and value deep connections with others. INFPs are driven by a strong sense of purpose and a desire to make a positive impact on the world.",
+                    10: "*INTJ(The Architect)*: Strategic and visionary, INTJs are natural leaders who excel at developing and executing plans. They are highly analytical and enjoy finding new and innovative solutions to problems. INTJs are driven by a strong sense of purpose and a desire to make a positive impact on the world.",
+                    11: "*INTP(The Architect)*: They are creative, innovative, and logical problem-solvers. They enjoy exploring new ideas and theories, and are not afraid to challenge traditional thinking.",
+                    12: "*ISFJ(The Protector)*: They are responsible, reliable, and nurturing individuals. They have a strong sense of duty and put the needs of others before their own. They value tradition and stability.",
+                    13: "*ISFP(The Composer)*: They are sensitive, imaginative, and spontaneous individuals. They are often artistic, and enjoy living in the moment. They value their independence and freedom.",
+                    14: "*ISTJ(The Inspector)*: They are practical, dependable, and methodical individuals. They value order, stability, and structure, and are committed to their responsibilities. They are often the backbone of organizations.",
+                    15: "*ISTP(The Craftsman)*: They are hands-on individuals who enjoy taking things apart and figuring out how they work. They are confident, independent, and resourceful, and value action and results over theoretical ideas."
                 }
                 return switcher.get(y, "Invalid class")
 
@@ -133,10 +133,8 @@ def app():
 
         # Call the switch case function
             result = switch_case(predicted_class)
-
         # Display the prediction to the user
-            st.success("Your personality type is {}".format(result), icon='✅')
-        # Display MBTI Accordian
+            st.success("*Your personality type is* {}".format(result), icon='✅')
             mbti_types = {
     "I" : "  INTROVERSION  ",
     "E" : "  EXTROVERSION  ",
@@ -146,7 +144,6 @@ def app():
     "F" : "  FEELING  ",
     "J" : "  JUDGING  ",
     "P" : "  PERCIEVING  ",
-
     }
             st.write(" ")
             st.write(" ")
@@ -160,6 +157,8 @@ def app():
         except Exception as e:
             st.error('Please provide a response to the textual question.')
 
+            # Add an accordion to display additional information
+
 
 def collect_responses(questions, options, text_questions):
     st.subheader("MCQ Questions")
@@ -171,6 +170,7 @@ def collect_responses(questions, options, text_questions):
         index_of_response = options.index(response) + 1
         responses.append(index_of_response)
 
+    
     st.write(" ")
     st.write("-----------------------------------------------------")
     st.header("Textual Questions: ")
@@ -181,49 +181,50 @@ def collect_responses(questions, options, text_questions):
         response = st.selectbox(
             text_questions[i]['question'], text_questions[i]['options'])
         text_responses.append(response)
-
     # Collect input for the fifth textual question using text_input
     text_response_string = ''
-    
+
     text_response = st.text_input('Describe a challenging situation you have faced in the past and how you overcame it.',max_chars=600,placeholder="Enter Answer Here")
     if text_response:
         text_response_string = text_response
 
-    # Combine all the textual responses into a single string
-    text_response_string = ' '.join(text_responses) + ' ' + text_response_string
 
-    # Remove URLs using regular expressions
-    text_response_string = re.sub(r"http\S+", "", text_response_string)
+        # Combine all the textual responses into a single string
+        text_response_string = ' '.join(text_responses) + ' ' + text_response_string
 
-    # Remove special characters using regular expressions
-    text_response_string = re.sub(r"[^a-zA-Z0-9]+", " ", text_response_string)
+        # Remove URLs using regular expressions
+        text_response_string = re.sub(r"http\S+", "", text_response_string)
 
-    # Tokenize the text_response_string into words
-    words = nltk.word_tokenize(text_response_string)
+        # Remove special characters using regular expressions
+        text_response_string = re.sub(r"[^a-zA-Z0-9]+", " ", text_response_string)
 
-    # Remove stop words using NLTK's English stop words list
-    stop_words = set(stopwords.words("english"))
-    words = [word for word in words if word.lower() not in stop_words]
+        # Tokenize the text_response_string into words
+        words = nltk.word_tokenize(text_response_string)
 
-    # Join the words back into a string
-    text_response_string = " ".join(words)
+        # Remove stop words using NLTK's English stop words list
+        stop_words = set(stopwords.words("english"))
+        words = [word for word in words if word.lower() not in stop_words]
 
-    # Initialize the TF-IDF Vectorizer with 32 features
-    tfidf = TfidfVectorizer(max_features=32)
+        # Join the words back into a string
+        text_response_string = " ".join(words)
 
-    # Fit and transform the textual data
-    result_tfidf = tfidf.fit_transform([text_response_string]).toarray()
+        # Initialize the TF-IDF Vectorizer with 32 features
+        tfidf = TfidfVectorizer(max_features=32)
 
-    # Append the textual questions answer to numerical questions answer
-    responses.extend(result_tfidf.tolist())
+        # Fit and transform the textual data
+        result_tfidf = tfidf.fit_transform([text_response_string]).toarray()
 
-    # Extend the last list in the array to the existing list
-    responses.extend(responses.pop())
+        # Append the textual questions answer to numerical questions answer
+        responses.extend(result_tfidf.tolist())
 
-    # Convert the user's answers list to a numpy array
-    responses = np.array(responses).reshape(1, -1)
+        # Extend the last list in the array to the existing list
+        responses.extend(responses.pop())
 
-    return responses
+        # Convert the user's answers list to a numpy array
+        responses = np.array(responses).reshape(1, -1)
+
+        return responses
+
 
 
 def load_model(file_path):
@@ -232,8 +233,10 @@ def load_model(file_path):
     return loaded_model
 
 
+
 def preprocess_input(responses):
     return np.array(responses).reshape(1, -1)
+
 
 
 if __name__ == '__main__':
